369dfd1518277a4dd9d4b268a4c1a0912ccf4160
Awspilot <awspilot@Adrians-MacBook.local>
2019-11-13 12:07:18 +0200

    +

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	node: false,
+	mode: 'production',
+	target: 'node',
+	context: path.resolve(__dirname, 'src'),
+	optimization: {
+		minimize: true,
+		minimizer: [new UglifyJsPlugin({
+			//uglifyOptions: { ecma: 7 },
+			//sourceMap: true,
+			include: /\.min\.js$/
+		})]
+	},
+	plugins: [
+	],
+	entry: {
+		'awspilot-dynamodb-util': path.resolve(__dirname, './src/index.js'),
+		'awspilot-dynamodb-util.min': path.resolve(__dirname, './src/index.js')
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+		library: 'ractive-dynamodb-util',
+
+		// var, this, window, umd
+		libraryTarget: 'umd',
+		libraryExport: 'default',
+		umdNamedDefine: true   // Important
+	},
+	externals: {
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: 'babel-loader'
+			}
+		]
+	}
+}
